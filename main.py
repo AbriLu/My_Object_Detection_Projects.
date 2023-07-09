c7b63975b4299229b504a09462dd98a5615ad03e
AbriLu <108410162+AbriLu@users.noreply.github.com>
2023-07-09 12:55:16 +0200

    Car Counter Project - Initial car tracking implementation_

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
+import math
+import numpy
 import numpy as np
+
+arr = np.array(-1)
+
+for i in range(5):
+    arr = numpy.vstack((arr, np.array(i)))
+# arr.reshape()
+arr = np.reshape(arr, (-1,))
+e = 9
+h = 7
+
+print(math.pi)
 
